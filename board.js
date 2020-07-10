e4825fee60328f5e66b05156cb41a084b0a43ca9
Asapanna Rakesh <rakeshark22@gmail.com>
2020-07-11 02:02:29 +0530

    changed to random play

--- board.js
+++ board.js
@@ -71,5 +71,16 @@ class Board {
                 if (this.matrix[i][j] == ''){ return [i, j] }
             }
         }
+    return null
+    }
+
+    allEmptyInRowMajor(){
+        var list = []
+        for(let i=0; i<3; i++){
+            for(let j=0; j<3; j++){
+                if (this.matrix[i][j] == '') {list.push([i, j])}
+            }
+        }
+        return list
     }
 }
--- minimax.js
+++ minimax.js
@@ -26,58 +26,44 @@ function randomMove(board){
     console.log(idx)
     return idx
 }
+/*
+var matCopy     = JSON.parse(JSON.stringify(board.matrix)) // deep copy
+var boardCopy   = new Board(matCopy)
+*/
 
-function returnBestMove(board){
-    // creeate deep copy of board as we are going to do inplace
+function returnBestMove(board){ 
+    // own copy of board
     var matCopy     = JSON.parse(JSON.stringify(board.matrix)) // deep copy
     var boardCopy   = new Board(matCopy)
 
-    var bestScore = -Infinity
-    var [bestScore, i, j] = mmx(boardCopy, score=bestScore, maximizing=true)
+    var emptys      = boardCopy.allEmptyInRowMajor()
+    var score       = -Infinity
+    
+    emptys.forEach((item, idx)=>{
 
-    console.log(i,j, 'idx:', vec2idx(i,j))
-    return vec2idx(i,j)
+        var [i, j] = item
+        score = minimax(boardCopy, [score, i, j], maximizing=true)
+
+        console.log(i, j, score)
+    })
 }
 
-function mmx(_board, score ,maximizing=true){
+function minimax(board, data, maximizing){
+    board.update(data[1], data[2], minmax2turn(maximizing))
     
-    var [i, j] = _board.nextMoveInRowMajor() // get next free locn
-    
-    // if maximizing, 'x's turn, if minimizing, 'o's turn
-    var turn = null
-    if (maximizing){ turn = 'x' } else { turn = 'o' }
-    console.log('mmx ponder', i, j, turn)
-
-    // update (in place)
-    _board.update(i, j, turn) 
-    
-    // check if -1(o wins), 0(tie), +1(x wins), null(game in progress)
-    var win = _board.winner()
-
-    // base condition(s)
-    // -----------------
-    if ((win == 1 && maximizing==true) || (win == 1 && maximizing==false)) {
-        // if, in x's turn (maximising) x won (or)
-        // if in o's turn(minimizing) o lost
-        return [10, i, j]
-    }
-    if ((win == -1 && maximizing==true) || (win == -1 && maximizing==false)) {
-        // if, in x's turn (maximising) x lost (or)
-        // if in o's turn(minimizing) o won
-        return [-10, i, j]
-    }
-    if ((win == 0 && maximizing==true) || (win == 0 && maximizing==false)) {
-        return [0, i, j] // same for tie
-    }
-
-
-    // esle if null(game in progress)
-    var retScore = mmx(_board, score=score, (!maximizing)) // false<->true recursively min<->maz
-    
-    if (retScore > score) { return [retScore, i, j] }
-    else {return [score, i, j]}
+    // base conditon
 }
 
+
+
+const minmax2turn = (maxOrMin) => {
+    if (maxOrMin === true){
+        return 'x'
+    } else {
+        // false (min)
+        return 'o'
+    }
+}
 
 function vec2idx(i, j){
     if (i === 0 && j == 0) { return 0 }
